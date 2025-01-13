c5bf21eedcf395ed7e499ddce9dd45fbaa5a9b51
Goulven BOURVEAU <goulven.bourveau@gmail.com>
2025-01-13 11:20:55 +0100

    updating the 'clean.sh' script for better cleaning

--- clean.sh
+++ clean.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
 rm -rf ./volumes/*
 mkdir ./volumes/{config,logs,data}
 touch ./volumes/{config,logs,data}/.gitkeep
+
+rm -rf ./restore/*
+mkdir ./restore/{.in_progress,archive}
+touch ./restore/{.in_progress,archive}/.gitkeep
+
+find ./backups/ -mindepth 1 -regextype posix-extended -not -regex '^\./backups/[0-9]{14}_v[0-9]+\.[0-9]+\.[0-9]+_gitlab_backup\.tar$' -print
+touch ./backups/.gitkeep
