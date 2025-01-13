0d6c9a3ff218cc176a4ec6cc7b1c27a93e76a918
Goulven BOURVEAU <goulven.bourveau@gmail.com>
2025-01-13 11:21:21 +0100

    forgot to remove debug

--- clean.sh
+++ clean.sh
@@ -8,5 +8,5 @@ rm -rf ./restore/*
 mkdir ./restore/{.in_progress,archive}
 touch ./restore/{.in_progress,archive}/.gitkeep
 
-find ./backups/ -mindepth 1 -regextype posix-extended -not -regex '^\./backups/[0-9]{14}_v[0-9]+\.[0-9]+\.[0-9]+_gitlab_backup\.tar$' -print
+find ./backups/ -mindepth 1 -regextype posix-extended -not -regex '^\./backups/[0-9]{14}_v[0-9]+\.[0-9]+\.[0-9]+_gitlab_backup\.tar$' -remove
 touch ./backups/.gitkeep
